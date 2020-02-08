3d56a731bf962099c3310955a6d1c73a8ed807c5
dmitriy <vardmev@gmail.com>
2020-02-08 21:48:06 +0400

    Add Users fields

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,16 @@
 class UsersController < ApplicationController
   def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(first_name: params[:user][:first_name], last_name: params[:user][:last_name])
+
+    redirect_to user_path(@user)
+  end
+
+  def show
+    @user = User.find(params[:id])
   end
 end
